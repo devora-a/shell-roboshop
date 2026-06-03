ce754bfe7dd51f7ccd1cf1c41acc23b20b49283a
devora-a <aravinddharla6@gmail.com>
2026-06-03 13:29:41 +0200

    add.doc

---
+++ 05-catalogue.sh
@@ -0,0 +1,78 @@
+#!bin/bash
+
+LOGS_FOLDER="var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
+
+USER_ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+TIMESTAMP=$(date "+%y-%m-%d %H:%M:%S")
+
+if [ $USER_ID -ne 0 ]; then
+  echo -e "$TIMESTAMP [ERROR] $R please run this script with root access $N" | tee -a $LOGS_FILE
+  exit 1
+  fi
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+fi
+
+}
+
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Creating roboshop user"
+else
+    echo "system user roboshop already created ... $Y skipping $N"
+fi
+
+rm -rf /app &>>$LOGS_FILE
+VALIDATE $? "Removing existing code"
+
+rm -rf /tmp/catalogue.zip 
+VALIDATE $? "Removing catalogue zip"
+
+mkdir -p /app &>>$LOGS_FILE
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+cd /app 
+unzip /tmp/catalogue.zip
+VALIDATE $? "Downloading and extracting catalogue code"
+
+npm install &>>$LOGS_FILE
+VALIDATE $? "Installing catalogue dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Created systemctl service"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding MongoDB repo"
+
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+VALIDATE $? "Installing MongoDB client"
+
+INDEX=$(mongodh --host mongodb.arrud.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -lt 0]; then
+    mongosh --host mongodb.arrud.online < /app/db/mater-data.js &>>$LOGS_FILE
+VALIDATE $? "Loading products"
+else
+    echo "products already loaded ... $Y skipping $N"
+fi
+
+systemctl enable catalogue &>>$LOGS_FILE
+systemctl restart catalogue &>>$LOGS_FILE
+VALIDATE $? "reStarting and enabling catalogue service"
+
+
---
+++ roboshop-v2.sh
@@ -0,0 +1,70 @@
+#!bin/bash
+
+### validation ###
+if [ $# -lt 2 ]; then
+  echo -e "$R ERROR: : Atleast 2 arguments required $N"
+  echo "USAGE: $0 [create/delete] [instance1] [instance2...]"
+  exit 1
+fi
+
+ACTION=$1
+shift # first argument will be removed
+
+if [ "$ACTION" != "create" ] && [ "$ACTION" != "delete" ]; then
+    echo -e "$R ERROR: : First argument must be either 'create' or 'delete' $N"
+    echo "UASGE: $0 [create/delete] [instance1] [instance2...]"
+    exit 1
+fi
+
+get_instance_id(){
+    name=$1
+    aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text
+
+}    
+
+for instance in $@
+do
+    INSTANCE_ID=$(get_instance_id $instance)
+    if [ $ACTION == "create" ]; then
+        if [ $INSTANCE_ID == "None" ]; then
+        else
+            echo "roboshop-$instance already running: $INSTANCE_ID"
+        fi
+
+        # update R53 record
+        if [ $instance == "frountend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+         --query 'Reservations[*].Instances[*].PublicIpAddress' \
+         --output text
+        )
+        R53_RECORD="$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+         --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+         --output text
+        )
+        R53_RECORD="$instance.$DOMAIN_NAME"
+    fi
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+       {   
+            "Comment": "Updating A record to new IP",
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": "'$R53_RECORD'",
+                        "Type": "A",
+                        "TTL": 1,
+                        "ResourceRecords": [
+                            {
+                                "Value": "'$IP'"
+                    }   ]   }
+                }
+            ]
+                
+        }
+    '
+done
