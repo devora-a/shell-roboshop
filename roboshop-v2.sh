36ca96a78fe74181277157cc7b032ebd09b05052
devora-a <aravinddharla6@gmail.com>
2026-06-03 13:43:31 +0200

    add.doc

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -27,7 +27,7 @@ do
     INSTANCE_ID=$(get_instance_id $instance)
     if [ $ACTION == "create" ]; then
         if [ $INSTANCE_ID == "None" ]; then
-        else
+    else
             echo "roboshop-$instance already running: $INSTANCE_ID"
         fi
 
