66df7a206163794c24a9916229e4d5b8a072e235
devora-a <aravinddharla6@gmail.com>
2026-06-04 17:34:20 +0200

    doc.add

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -89,4 +89,11 @@ do
                 
         }
     '
+    else
+        if [ $INSTANCE_ID == "None" ]; then 
+            echo "$instance already destroyed nothing to do"
+        else
+            aws ec2 terminate-instances --instance-ids $INSTANCE_ID
+            echo "Terminated Instance: $INSTANCE_ID"
+        fi
 done
