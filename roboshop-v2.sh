38d82a3db99f45d3ecbe0b474fcf7fe8c53040f6
devora-a <aravinddharla6@gmail.com>
2026-06-06 13:30:00 +0200

    doc.add

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -29,7 +29,7 @@ fi
 
 get_instance_id(){
     name=$1
-    aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-instance" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text
+    aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$instance" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text
 
 }    
 
