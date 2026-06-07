f01fec475ef7d14fe257d5a88e4a2787317a5320
devora-a <aravinddharla6@gmail.com>
2026-06-07 14:06:23 +0200

    doc.add

---
+++ roboshop-v3.sh
@@ -0,0 +1,115 @@
+#!bin/bash
+
+AMI_ID=ami-0220d79f3f480ecf5
+ZONE_ID=Z04138223ALQPP4SRQZFJ
+DOMAIN_NAME=arrud.online
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+ALL_INSTANCES="mongodb redis mysql rabbitmq catalogue user cart shipping payment frontend"
+
+### validation ###
+if [ $# -lt 2 ]; then
+  echo -e "$R ERROR: : Atleast 2 arguments required $N"
+  echo "USAGE: $0 [create/delete] [instance1] [instance2...]"
+  exit 1
+fi
+
+ACTION=$1
+shift # first argument will be removed
+
+if [ "$ACTION" != "create" ] && [ "$ACTION" != "delete" ]; then
+    echo -e "$R ERROR: : First argument must be either 'create' or 'delete' $N"
+    echo "UASGE: $0 [create/delete] [instance1] [instance2...]"
+    exit 1
+fi
+     
+# if "all" is passed, expand to full list (reversed for delete)
+if [ "$1" == "all" ]; then
+    if [ "$ACTION" == "create" ]; then
+          INSTANCES="$ALL_INSTANCES"
+        else
+            INSTANCES=$(echo $ALL_INSTANCES | tr ' ' '\n' | tac | tr '\n' ' ')
+        fi
+else
+    INSTANCES="$@"
+fi
+
+
+get_instance_id(){
+    name=$1
+    aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text
+
+}    
+
+for instance in $@
+do  
+    INSTANCE_ID=$(get_instance_id $instance)
+    if [ $ACTION == "create" ]; then
+        if [ $INSTANCE_ID == "None" ]; then 
+            echo "Launching instance: roboshop-$instance"
+            INSTANCE_ID=$(aws ec2 run-instances \
+                --image-id $AMI_ID \
+                --instance-type t3.micro \
+                --security-groups "roboshop-common" "roboshop-$instance" \
+                --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+                --query 'Instances[0].InstanceId' \
+                --output text
+                )
+            echo "Launched Instance: $INSTANCE_ID"
+        else
+            echo -e "roboshop-$instance already running: $INSTANCE_ID $N"
+        fi
+
+        # update R53 record
+        if [ $instance == "frountend" ]; then
+             IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+             --query 'Reservations[*].Instances[*].PublicIpAddress' \
+             --output text
+             )
+            R53_RECORD="$DOMAIN_NAME"
+        else
+           IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+             --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+             --output text
+             )
+            R53_RECORD="$instance.$DOMAIN_NAME"
+        fi
+
+
+     
+    
+      aws route53 change-resource-record-sets \
+      --hosted-zone-id $ZONE_ID \
+      --change-batch '
+       {   
+            "Comment": "Updating A record to new IP",
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": "'$R53_RECORD'",
+                        "Type": "A",
+                        "TTL": 1,
+                        "ResourceRecords": [
+                            {
+                                "Value": "'$IP'"
+                    }   ]   }
+                }
+            ]
+                
+        }
+    '
+      echo -e "Updated R53 record: $R53_RECORD -> $IP $N"
+    
+    else
+        if [ $INSTANCE_ID == "None" ]; then 
+            echo "$instance already destroyed, nothing to do"
+        else
+            aws ec2 terminate-instances --instance-ids $INSTANCE_ID
+            echo "Terminated Instance: $instance"
+        fi
+    fi
+done
