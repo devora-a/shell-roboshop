c0a86f3bf426347f5da7a7d53003d2a5b74cc4d2
devora-a <aravinddharla6@gmail.com>
2026-06-08 12:44:01 +0200

    doc.add

--- roboshop.sh
+++ roboshop.sh
@@ -6,21 +6,21 @@ DOMAIN_NAME=arrud.online
 
 for instance in $@
 do
-     echo "Launching instance: $instance"
-     INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id ami-0220d79f3f480ecf5 \
-        --instance-type t3.micro \
-        --security-groups "roboshop-common" "roboshop-$instance" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
-        --query 'Instances[0].InstanceId' \
-        --output text
-     )
+    echo "Launching instance: $instance"
+    INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id ami-0220d79f3f480ecf5 \
+    --instance-type t3.micro \
+    --security-groups "roboshop-common" "roboshop-$instance" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text
+    )
     echo "Instance ID: $INSTANCE_ID"
 
     if [ $instance == "frountend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-         --query 'Reservations[*].Instances[*].PublicIpAddress' \
-         --output text
+        --query 'Reservations[*].Instances[*].PublicIpAddress' \
+        --output text
         )
         R53_RECORD="$DOMAIN_NAME"
     else
